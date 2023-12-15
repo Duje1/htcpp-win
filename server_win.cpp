033f619e636f3b401b75b2bf72b4187f24f59eab
Duje1 <duje1mandalinic@gmail.com>
2023-12-15 18:40:46 +0100

    Fixed "index.html", removed fixed address when starting the server

--- server_win.cpp
+++ server_win.cpp
@@ -1,9 +1,17 @@
 #include "http_tcpServer_win.h"
-
+#include <iostream>
 int main()
 {
     using namespace http;
-    TcpServer server = TcpServer("127.0.0.1", 8080);
+
+    std::string ip_adrr;
+    int port;
+    std::cout<<"Enter the ip address: ";
+    std::cin>>ip_adrr;
+    std::cout<<"Enter the port (eg. 8080): ";
+    std::cin>>port;
+
+    TcpServer server = TcpServer(ip_adrr, port);
     server.startListen();
     return 0;
 }
